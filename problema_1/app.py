33e8d38e69e53719c2c7d1dbc5903341b9009bd1
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-03-29 18:03:22 -0300

    Adicionando função travessia em ordem

--- problema_1/app.py
+++ problema_1/app.py
@@ -4,5 +4,13 @@ class No:
         self.esquerda = None
         self.valor = chave
 
+def main():
+    def travessia_em_ordem(root):
+        if root:
+            travessia_em_ordem(root.esquerda)
+            print(root.valor)
+            travessia_em_ordem(root.direita)
+
+
 if __name__ == "__main__":
     main()
