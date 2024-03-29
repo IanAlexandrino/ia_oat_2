6d652d03738ba34c2c4402c4242e6fc4ebd84ff5
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-03-29 18:07:03 -0300

    Adicionando função inserção

--- problema_1/app.py
+++ problema_1/app.py
@@ -11,6 +11,21 @@ def main():
             print(root.valor)
             travessia_em_ordem(root.direita)
 
+    def insercao(root, chave):
+
+        if root is None:
+            return No(chave)
+        
+        else:
+
+            if root.valor < chave:
+                root.direita = insercao(root.direita, chave)
+
+            else:
+                root.esquerda = insercao(root.esquerda, chave)
+                
+        return root
+
 
 if __name__ == "__main__":
     main()
